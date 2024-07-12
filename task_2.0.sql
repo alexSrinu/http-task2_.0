faa94665bcb90e2d74eb6cb36a51b6ce91adfc7b
alexSrinu <alekhyavantrapati41@gmail.com>
2024-07-12 16:58:48 +0530

    Add files via upload

--- task_2.0.sql
+++ task_2.0.sql
@@ -1,50 +1,77 @@
 --------table creation--------
-create table TB_Hostel
+create table TB_Hostel1a
 (
 Id int primary key identity (1,1),
 Name varchar(30),
 Password varchar(20),
 MobileNo varchar(20),
 Email varchar(20),
-CountryId int,
+CountryId varchar(15),
 Gender varchar(6),
 Hobbies varchar(100),
 DateOfBirth date
 );
 ALTER TABLE TB_Hostel
+add IsActive BIT
+ALTER TABLE TB_Hostel
 ADD CONSTRAINT PK_TB_Hostel_Name PRIMARY KEY (Name);
+select * from TB_Hostel1a
+alter table 1a
+ 
+drop column IsActive
+
 
 
 ----------------insert stored procedure---------------
-create procedure proc_insert_TB_Hostel
+create procedure proc_insert_TB_Hostel1a
 (@Name varchar(30),@Password varchar(20),
 @MobileNo varchar(20),@Email varchar(20),@CountryId varchar(15),
 @Gender varchar(6),@Hobbies varchar(100),@DateOfBirth date,@Id int out)
 as begin
-insert into TB_Hostel values (@Name,@Password,@MobileNo,@Email,@CountryId,@Gender,@Hobbies,@DateOfBirth)
+insert into TB_Hostel1a values (@Name,@Password,@MobileNo,@Email,@CountryId,@Gender,@Hobbies,@DateOfBirth)
 set @Id=@@IDENTITY
 END
 ------------------checking username and mobileno and emial not repeated----------------------------
-CREATE PROCEDURE spCheckUserExists
-    @UserName NVARCHAR(50),
+CREATE PROCEDURE spCheckUserExists1d
+    
     @MobileNo NVARCHAR(15),
     @Email NVARCHAR(50)
 AS
 BEGIN
     SELECT COUNT(*) 
-    FROM TB_Hostel
-    WHERE Name = @UserName OR MobileNo = @MobileNo OR Email = @Email;
+    FROM TB_Hostel1a
+    WHERE  MobileNo = @MobileNo OR Email = @Email;
+END
+CREATE PROCEDURE spCheckUserExists2D
+    
+    @MobileNo NVARCHAR(15)
+    
+AS
+BEGIN
+    SELECT COUNT(*) 
+    FROM TB_Hostel1a
+    WHERE  MobileNo = @MobileNo ;
+END
+CREATE PROCEDURE spCheckUserExists3d
+    
+    
+    @Email NVARCHAR(50)
+AS
+BEGIN
+    SELECT COUNT(*) 
+    FROM TB_Hostel1a
+    WHERE   Email = @Email;
 END
 ----------------------how to see the table-----
-select * from TB_Hostel
+select * from TB_Hostel1a
 --------------------login stored procedure---------------
 create procedure proc_Login_Student(@Name varchar(30),@Password varchar(20),@MobileNo varchar(20),@Email varchar(20))
 as begin
-select count(*) from TB_Hostel where Name=@Name or MobileNo=@MobileNo or Email=@Email and Password=@Password
+select count(*) from TB_Hostel1a where Name=@Name or MobileNo=@MobileNo or Email=@Email and Password=@Password
 end;
-create procedure proc_Login_Student1(@Password varchar(20),@Email varchar(20))
+create procedure proc_Login_Student1b(@Password varchar(20),@Email varchar(20))
 as begin
-select count(*) from TB_Hostel where  Email=@Email and Password=@Password
+select count(*) from TB_Hostel1a where  Email=@Email and Password=@Password
 end;
 create procedure proc_Login_Student100(@Id int,@Password varchar(20),@Email varchar(20))
 as begin
@@ -63,22 +90,22 @@ CountryId=@CountryId,Gender=@Gender,Hobbies=@Hobbies,DateOfBirth=@DateOfBirth
 where Email=@Email and Password=@Password
 end
 --------------------update without datetime stored procedure---------------------
-create procedure proc_Update_Student1A
+create procedure proc_Update_Student1B
 (@Name varchar(30),@Password varchar(20),
 @MobileNo varchar(20),@Email varchar(20),@CountryId varchar(15),
 @Gender varchar(6),@Hobbies varchar(100), @DateOfBirth date,@Id int )
 as begin
 update TB_Hostel set Name=@Name,Password=@Password,MobileNo=@MobileNo,Email=@Email,
-CountryId=@CountryId,Gender=@Gender,Hobbies=@Hobbies,DateOfBirth=@DateOfBirth
-where Email=@Email and Password=@Password
+CountryId=@CountryId,Gender=@Gender,Hobbies=@Hobbies,DateOfBirth=@DateOfBirth,IsActive=1
+where Email=@Email and Password=@Password 
 end
-create procedure proc_Update_Student111
-(@Name varchar(30),@Password varchar(20),
+create procedure proc_Update_Student100B
+(@Name varchar(30),
 @MobileNo varchar(20),@Email varchar(20),@CountryId varchar(15),
 @Gender varchar(6),@Hobbies varchar(100), @DateOfBirth date,@Id int )
 as begin
-update TB_Hostel set Name=@Name,Password=@Password,MobileNo=@MobileNo,Email=@Email,
-CountryId=@CountryId,Gender=@Gender,Hobbies=@Hobbies,DateOfBirth=@DateOfBirth
+update TB_Hostel set Name=@Name,MobileNo=@MobileNo,Email=@Email,
+CountryId=@CountryId,Gender=@Gender,Hobbies=@Hobbies,DateOfBirth=@DateOfBirth,IsActive=1
 where Id=@Id
 end
 -----------------retrive stored procedure------------------
@@ -120,6 +147,25 @@ end
 exec proc_Delete_Admin1 @Id=3
 -------------------------checking some queries---------------------
 select * from TB_Hostel where id=(select id from TB_Hostel where Email='patlu@gmail.com ' and Password='123456')
+ALTER TABLE TB_Hostel 
+ADD CONSTRAINT DF_Users_IsActive DEFAULT 1 FOR IsActive;
+------------------------------delete----------------------
+CREATE PROCEDURE DeleteUser
+    @Id INT
+AS
+BEGIN
+    UPDATE TB_Hostel
+    SET IsActive = 0
+    WHERE Id = @Id;
+END
+exec DeleteUser
+@Id=8
+select * from TB_Hostel
+proc_Update_Student100B
+delete from TB_Hostel where Id=1015
+
+
+
 
 
 
